8fd106065fc80aca6a731136fd93d2c119d40222
Vicente de Alencar <vicentedealencar@gmail.com>
2018-06-06 01:39:48 -0300

    Adding snapshot tests via kit

---
+++ examples/index.js
@@ -0,0 +1,2 @@
+export { default as BasicButton } from './basic.Button.example'
+export { default as LoadingButton } from './loading.Button.example'
---
+++ tests/Button.test.js
@@ -0,0 +1,4 @@
+import snapshotExamples from '@compositor/kit-snapshot'
+import * as examples from '../examples'
+
+snapshotExamples(examples)
